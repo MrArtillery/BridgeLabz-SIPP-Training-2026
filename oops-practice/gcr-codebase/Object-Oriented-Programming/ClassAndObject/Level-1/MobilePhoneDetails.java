799c1a9bdd89fd641398278ff8157e9b9c5154ce
MrArtillery <ankursingh06.12.2003@gmail.com>
2026-07-01 09:32:20 +0530

    [Ankur] Add: Mobile phone information system

---
+++ oops-practice/gcr-codebase/Object-Oriented-Programming/ClassAndObject/Level-1/MobilePhoneDetails.java
@@ -0,0 +1,33 @@
+class MobilePhone {
+
+    String brand;
+    String model;
+    double price;
+
+    // Constructor
+    public MobilePhone(String brand, String model, double price) {
+        this.brand = brand;
+        this.model = model;
+        this.price = price;
+    }
+
+    // Method to display phone details
+    public void displayDetails() {
+        System.out.println("Brand : " + brand);
+        System.out.println("Model : " + model);
+        System.out.println("Price : ₹" + price);
+    }
+}
+
+public class MobilePhoneDetails {
+
+    public static void main(String[] args) {
+
+        MobilePhone phone = new MobilePhone(
+                "Samsung",
+                "Galaxy S24",
+                79999);
+
+        phone.displayDetails();
+    }
+}
